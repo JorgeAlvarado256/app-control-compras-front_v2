cb7dca990772275ca8e582dd494eb54c02bc0d8b
JorgeAlvarado256 <jorge.alvarado19@inacapmail.cl>
2024-07-08 22:09:30 -0400

    cambio de servidor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@ export const environment = {
     // apiUrl: 'http://localhost:3000/api/v1', // URL de la API en desarrollo
     //apiUrl: 'https://controlcomprasserver-production.up.railway.app/api/v1'
     // apiUrl: 'https://courageous-scarf-bee.cyclic.cloud/api/v1'
-    // apiUrl:'https://controlcomprasserver-develop-1.onrender.com/api/v1'
     apiUrl:'https://controlcomprasserver-dddf2-default-rtdb.firebaseio.com/api/v1'
   };
   
