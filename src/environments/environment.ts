e690aeaa780b5798940ac2da896cbc3d64745ff9
JorgeAlvarado256 <jorge.alvarado19@inacapmail.cl>
2024-07-08 20:54:41 -0400

    deploy sistema y firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
     // apiUrl: 'http://localhost:3000/api/v1', // URL de la API en desarrollo
     //apiUrl: 'https://controlcomprasserver-production.up.railway.app/api/v1'
     // apiUrl: 'https://courageous-scarf-bee.cyclic.cloud/api/v1'
-    apiUrl:'https://controlcomprasserver-develop-1.onrender.com/api/v1'
+    // apiUrl:'https://controlcomprasserver-develop-1.onrender.com/api/v1'
+    apiUrl:'https://console.firebase.google.com/u/0/project/controlcomprasserver-dddf2/database/controlcomprasserver-dddf2-default-rtdb/data/~2F?hl=es-419/api/v1'
   };
   
