0264cda7d9befd1638d367c55857e38ca4db6751
JorgeAlvarado256 <jorge.alvarado19@inacapmail.cl>
2024-07-08 21:12:03 -0400

    apiUrl:'https://controlcomprasserver-dddf2-default-rtdb.firebaseio.com/api/v1'  en enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: true,
     // apiUrl: 'https://controlcomprasserver-production.up.railway.app/api/v1'
     //apiUrl: 'https://db-mysql-control-compras-c5a73be4bcfa.herokuapp.com/api/v1', // URL de la API en producción
+    apiUrl:'https://controlcomprasserver-dddf2-default-rtdb.firebaseio.com/api/v1'
+
   };
   
